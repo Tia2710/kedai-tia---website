0f4fe32c4eca01eb28d066f2a6b0155b9b1b5ccf
Tia2710 <tiaaam278@gmail.com>
2026-05-17 11:38:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle Class Aktif untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = () => {
+  searchForm.classList.toggle("active");
+};
